ce11fc25d99784cec57c95e3d81b485e3d8b86b4
christopher bolocan <chris.bolocan@gmail.com>
2026-02-23 20:43:02 +0100

    Mediatype arreglado

--- src/app/(protected)/productos/crear.tsx
+++ src/app/(protected)/productos/crear.tsx
@@ -88,7 +88,7 @@ export default function CrearProductoScreen() {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images','videos'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.8,
--- src/app/(protected)/productos/editar/[id].tsx
+++ src/app/(protected)/productos/editar/[id].tsx
@@ -124,7 +124,7 @@ export default function EditarProductoScreen() {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images','videos'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.8,
